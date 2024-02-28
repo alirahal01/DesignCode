4fd430d24cd790d4f579e8d00118de1eba343a8d
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2024-02-28 19:30:11 +0200

    Setup components and animations for certificate page

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,149 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by ali rahal on 26/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var show = false
+    @State var viewState = CGSize.zero
+    var body: some View {
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                
+            BackgroundView()
+                .background(show ? Color("card3") : Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotation3DEffect(
+                    .degrees(10), axis: (x: 10.0, y: 0, z: 0.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            BackgroundView()
+                .background(show ? Color("card4") : Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
+                .scaleEffect(0.95)
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotation3DEffect(
+                    .degrees(5), axis: (x: 10.0, y: 0, z: 0.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+            CardView()
+                .offset(x: viewState.width, y: viewState.height)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.show.toggle()
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            self.viewState = value.translation
+                            self.show = true
+                        })
+                        .onEnded({ value in
+                            self.viewState = .zero
+                            self.show = false
+                        })
+                )
+            BottomCardView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+            
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng to has achieved that UI Design course with approval from a Design+Code instructor")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
+    }
+}
---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by ali rahal on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
