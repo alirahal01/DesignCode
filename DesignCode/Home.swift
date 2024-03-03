f0705725908e86078ffd41c4b0c56f7b90ba3bb7
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2024-03-03 16:54:11 +0200

    Drag progess and tap background

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct Home: View {
     @State var showProfile = false
+    @State var viewState = CGSize.zero
+    
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
@@ -37,7 +39,7 @@ struct Home: View {
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y:20)
             .offset(y: showProfile ? -450 : 0)
             .rotation3DEffect(Angle(degrees:
-                showProfile ? -10 : 0),
+                                        showProfile ? Double(viewState.height) / 10 - 10 : 0),
                                       axis: (x: 10.0, y: 0.0, z: 0.0)
             )
             .scaleEffect(showProfile ? 0.9 : 1)
@@ -46,10 +48,28 @@ struct Home: View {
                                 blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
             MenuView()
-                .offset(y: showProfile ? 0 : 600)
+                .background(.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5,
                                    dampingFraction: 0.6,
                                     blendDuration: 0))
+                .onTapGesture {
+                    showProfile.toggle()
+                    
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            self.viewState = value.translation
+                        })
+                        .onEnded({ value in
+                            if self.viewState.height > 50 {
+                                self.showProfile = false
+                            }
+                            self.viewState = .zero
+                        })
+                )
         }
     }
 }
