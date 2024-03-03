fef782a36da6b0f8f72db738feb1cb51c7d25740
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2024-03-03 17:08:32 +0200

    Binding and screen size

---
+++ DesignCode/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by ali rahal on 03/03/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28,weight: .bold))
+                Spacer()
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    HomeView(showProfile: .constant(false))
+}
