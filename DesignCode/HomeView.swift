57e62cb67aad60fb74b9b8dc06af120446890c0c
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2024-03-04 22:12:23 +0200

    ScrollView and repeat

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -20,6 +20,16 @@ struct HomeView: View {
             }
             .padding(.horizontal)
             .padding(.top, 30)
+            .padding(.leading, 14)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0 ..< 3) { item in
+                        SectionView()
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
             Spacer()
         }
     }
@@ -27,4 +37,32 @@ struct HomeView: View {
 
 #Preview {
     HomeView(showProfile: .constant(false))
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, content: {
+                Text("Prototype designs in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image("Logo1")
+            })
+            Text("18 Sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
+    }
 }
