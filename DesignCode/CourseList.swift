33cef3865cd0f519dc0d0b064b6673798c4ce1b3
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2024-04-09 21:32:31 +0300

    Dynamic new view 33

--- DesignCode/CourseList.swift
+++ DesignCode/CourseList.swift
@@ -5,28 +5,30 @@
 //  Created by ali rahal on 14/03/2024.
 //
 
+
 import SwiftUI
 
 struct CourseList: View {
     @State var courses = courseData
     @State var active = false
     @State var activeIndex = -1
+    @State var activeView = CGSize.zero
     
     var body: some View {
         ZStack {
-            Color.black.opacity(active ? 0.5 : 0)
+            Color.black.opacity(Double(self.activeView.height/500))
                 .animation(.linear)
                 .edgesIgnoringSafeArea(.all)
             
             ScrollView {
                 VStack(spacing: 30) {
                     Text("Courses")
-                        .font(.largeTitle)
-                        .bold()
+                        .font(.largeTitle).bold()
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 30)
                         .padding(.top, 30)
                         .blur(radius: active ? 20 : 0)
+                    
                     ForEach(courses.indices, id: \.self) { index in
                         GeometryReader { geometry in
                             CourseView(
@@ -34,7 +36,8 @@ struct CourseList: View {
                                 course: self.courses[index],
                                 active: self.$active,
                                 index: index,
-                                activeIndex: self.$activeIndex
+                                activeIndex: self.$activeIndex,
+                                activeView: self.$activeView
                             )
                                 .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
                                 .opacity(self.activeIndex != index && self.active ? 0 : 1)
@@ -48,23 +51,26 @@ struct CourseList: View {
                 }
                 .frame(width: screen.width)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-                .statusBar(hidden: active ? true : false)
-                .animation(.linear)
             }
+            .statusBar(hidden: active ? true : false)
+            .animation(.linear)
         }
     }
 }
 
-#Preview {
-    CourseList()
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+    }
 }
 
 struct CourseView: View {
-    @Binding var show : Bool
+    @Binding var show: Bool
     var course: Course
     @Binding var active: Bool
-    var index : Int
-    @Binding var activeIndex : Int
+    var index: Int
+    @Binding var activeIndex: Int
+    @Binding var activeView: CGSize
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -79,14 +85,15 @@ struct CourseView: View {
                 Text("Minimal coding experience required, such as in HTML and CSS. Please note that Xcode 11 and Catalina are essential. Once you get everything installed, it'll get a lot friendlier! I added a bunch of troubleshoots at the end of this page to help you navigate the issues you might encounter.")
             }
             .padding(30)
-            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: CGFloat(show ? .infinity : 280.0), alignment: .top)
+            .frame(maxWidth: CGFloat(show ? .infinity : screen.width - 60), maxHeight: CGFloat(show ? .infinity : 280.0), alignment: .top)
             .offset(y: show ? 460 : 0)
-            .background(.white)
+            .background(Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .opacity(show ? 1 : 0)
-            VStack  {
-                HStack {
+            
+            VStack {
+                HStack(alignment: .top) {
                     VStack(alignment: .leading, spacing: 8.0) {
                         Text(course.title)
                             .font(.system(size: 24, weight: .bold))
@@ -98,10 +105,11 @@ struct CourseView: View {
                     ZStack {
                         Image(uiImage: course.logo)
                             .opacity(show ? 0 : 1)
+                        
                         VStack {
                             Image(systemName: "xmark")
                                 .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.white)
+                                .foregroundColor(.white)
                         }
                         .frame(width: 36, height: 36)
                         .background(Color.black)
@@ -114,15 +122,33 @@ struct CourseView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: .infinity)
-                    .frame(maxHeight: 140, alignment: .top)
+                    .frame(height: 140, alignment: .top)
             }
             .padding(show ? 30 : 20)
-            .padding(.top, show ? 30 : 0)
+            .padding(.top, CGFloat(show ? 30 : 0))
     //        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
-            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280 )
-            .background(Color(course.color))
+            .frame(maxWidth: CGFloat(show ? .infinity : screen.width - 60), maxHeight: CGFloat(show ? 460 : 280))
+                .background(Color(course.color))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color(course.color).opacity(0.3), radius: 20, x:0, y:20)
+                .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
+            .gesture(
+                show ?
+                DragGesture().onChanged { value in
+                    guard value.translation.height < 300 else { return }
+                    guard value.translation.height > 0 else { return }
+                    
+                    self.activeView = value.translation
+                }
+                .onEnded { value in
+                    if self.activeView.height > 50 {
+                        self.show = false
+                        self.active = false
+                        self.activeIndex = -1
+                    }
+                    self.activeView = .zero
+                }
+                : nil
+            )
             .onTapGesture {
                 self.show.toggle()
                 self.active.toggle()
@@ -132,9 +158,36 @@ struct CourseView: View {
                     self.activeIndex = -1
                 }
             }
+            
+            if show {
+//                CourseDetail(course: course, show: $show, active: $active, activeIndex: $activeIndex)
+//                    .background(Color.white)
+//                    .animation(nil)
+            }
         }
         .frame(height: show ? screen.height : 280)
+        .scaleEffect(1 - self.activeView.height / 1000)
+        .rotation3DEffect(Angle(degrees: Double(self.activeView.height / 10)), axis: (x: 0, y: 10.0, z: 0))
+        .hueRotation(Angle(degrees: Double(self.activeView.height)))
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .gesture(
+            show ?
+            DragGesture().onChanged { value in
+                guard value.translation.height < 300 else { return }
+                guard value.translation.height > 0 else { return }
+                
+                self.activeView = value.translation
+            }
+            .onEnded { value in
+                if self.activeView.height > 50 {
+                    self.show = false
+                    self.active = false
+                    self.activeIndex = -1
+                }
+                self.activeView = .zero
+            }
+            : nil
+        )
         .edgesIgnoringSafeArea(.all)
     }
 }
